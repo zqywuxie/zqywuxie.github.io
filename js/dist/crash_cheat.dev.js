e34226d135ef959d986d966c2c5148f8d7413ea4
zqywuxie <573905257@qq.com>
2022-08-28 12:17:35 +0800

    Site updated: 2022-08-28 12:17:34

---
+++ js/dist/crash_cheat.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    $('[rel="icon"]').attr('href', "/joke.ico");
+    document.title = '不要走再看看啊 🥲';
+    clearTimeout(titleTime);
+  } else {
+    $('[rel="icon"]').attr('href', "/favicon.ico");
+    document.title = '感谢观看 👻';
+    titleTime = setTimeout(function () {
+      document.title = OriginTitle;
+    }, 2000);
+  }
+});
